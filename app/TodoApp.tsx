0c50d883440fbfebfa02f9c4e53703ca2d815324
zzyyxxww <guidotarsia@gmail.com>
2022-03-30 19:10:34 -0300

    Move tasklist to comps/task-list

--- app/TodoApp.tsx
+++ app/TodoApp.tsx
@@ -1,7 +1,7 @@
 // import { useEffect } from 'react'
 // import { hydrateTodo, setDocument } from 'src/store'
 // import { DocumentSelector } from './DocumentSelector'
-import { TaskList } from 'app/comps/list'
+import { TaskList } from 'app/comps/task-list'
 
 export function TodoApp() {
   return <div className="flex h-full pt-4 h-full">
