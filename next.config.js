466c23e8286ec80264ea41a773e1576b57e2e9b2
zzyyxxww <guidotarsia@gmail.com>
2022-04-02 00:06:05 -0300

    Prepare for github pages push

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
+  basePath: process.env.NODE_ENV === 'production' ? '/todo' : '',
 }
 
 module.exports = nextConfig
