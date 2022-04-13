027ab998e7acaa062dca89722fc0e872eec25ed3
zzyyxxww <guidotarsia@gmail.com>
2022-04-13 14:40:00 -0300

    Make focus mode show next and previous tasks

--- app/utils/task/get-focused-indexes.test.ts
+++ app/utils/task/get-focused-indexes.test.ts
@@ -6,17 +6,17 @@ function task(indent: number) {
 
 test('getFocusedIndexes', () => {
   expect(getFocusedIndexes([
-    task(1), task(1), task(2), task(2), task(3), task(3),
-    task(3),
-  ], 5)).toStrictEqual([
-    false,   true,    false,   true,    false,    true,
-    false
+    task(1), task(1), task(2), task(2), task(3), task(4), task(3),
+    task(4), task(3),
+  ], 6)).toStrictEqual([
+    false,   true,    false,   true,    true,    false,  true,
+    true,    true,
   ])
-  // expect(getFocusedIndexes([
-  //   task(1), task(1), task(2), task(2), task(3), task(3),
-  //   task(4), task(5), task(5), task(4), task(4), task(3)
-  // ], 2)).toStrictEqual([
-  //   false,   true,    false,   true,    false,    true,
-  //   true,    true,    true,    true,    true,     false,
-  // ])
+  expect(getFocusedIndexes([
+    task(1), task(1), task(2), task(2), task(3), task(3),
+    task(4), task(5), task(5), task(4), task(4), task(3)
+  ], 2)).toStrictEqual([
+    false,   true,    true,    true,    false,   false,
+    false,   false,   false,   false,   false,   false,
+  ])
 })
--- app/utils/task/get-focused-indexes.ts
+++ app/utils/task/get-focused-indexes.ts
@@ -3,18 +3,27 @@ import { TaskModel } from 'app/types'
 export function getFocusedIndexes(tasks: TaskModel[], index: number): boolean[] {
   const indexes = tasks.map(() => false)
   indexes[index] = true
-  let lastIndent = tasks[index].indent
+  const originalIndent = tasks[index].indent
+  let lastIndent = originalIndent
+  let canHavePreviousSibling = true
   for (let i = index - 1; i > -1; i -= 1) {
     const { indent } = tasks[i]
-    if (indent < lastIndent) {
+    if (indent === originalIndent && canHavePreviousSibling) {
+      indexes[i] = true
+      canHavePreviousSibling = false
+    } else if (indent < lastIndent) {
       indexes[i] = true
       lastIndent = indent
+      canHavePreviousSibling = false
     }
   }
-  const { indent: mainIndent } = tasks[index]
+  let canHaveNextSibling = true
   for (let i = index + 1; i < tasks.length; i += 1) {
     const { indent } = tasks[i]
-    if (indent > mainIndent) {
+    if (indent === originalIndent) {
+      indexes[i] = true
+      break
+    } else if (indent > originalIndent) {
       indexes[i] = true
     } else {
       break
