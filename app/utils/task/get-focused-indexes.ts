30d5b2560c061a17f08b48f569a7ef7fb4e27410
gtarsia <guidotarsia@gmail.com>
2023-09-06 18:48:30 -0300

    Make focus mode only show current and children

--- app/utils/task/get-focused-indexes.ts
+++ app/utils/task/get-focused-indexes.ts
@@ -4,26 +4,10 @@ export function getFocusedIndexes(tasks: TaskModel[], index: number): boolean[] 
   const indexes = tasks.map(() => false)
   indexes[index] = true
   const originalIndent = tasks[index].indent
-  let lastIndent = originalIndent
-  let canHavePreviousSibling = true
-  for (let i = index - 1; i > -1; i -= 1) {
-    const { indent } = tasks[i]
-    if (indent === originalIndent && canHavePreviousSibling) {
-      indexes[i] = true
-      canHavePreviousSibling = false
-    } else if (indent < lastIndent) {
-      indexes[i] = true
-      lastIndent = indent
-      canHavePreviousSibling = false
-    }
-  }
   let canHaveNextSibling = true
   for (let i = index + 1; i < tasks.length; i += 1) {
     const { indent } = tasks[i]
-    if (indent === originalIndent) {
-      indexes[i] = true
-      break
-    } else if (indent > originalIndent) {
+    if (indent > originalIndent) {
       indexes[i] = true
     } else {
       break
